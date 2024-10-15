1dcc3cc2e00cb11a8e524bcb2d864046bc5f066e
김현준 <guo7404@naver.com>
2024-10-15 11:48:30 +0900

    .

--- js/week07_4.js
+++ js/week07_4.js
@@ -7,11 +7,12 @@ class Menu{
     intro(){
         console.log(`${this.title}: ${this.price}원 (재고량:${this.count}개)`);
     }
-    order(count2){
-        this.count -= count2;
+    order(count){
+        this.count -= count;
     }
 }
 let ramyeon = new Menu("라면",1000,100);
+ramyeon.count = 200;
 ramyeon.intro();// 라면: 1000원 (재고량:100개)
 ramyeon.order(2);//재고량에서 2개뺌
 ramyeon.intro();
